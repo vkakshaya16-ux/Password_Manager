1af0b90067a05e9a23768b560fcf08c2a52fdadc
Akshaya Priyanka <vkakshaya16@gmail.com>
2026-03-28 09:24:45 +0530

    first deploy

--- app.py
+++ app.py
@@ -174,6 +174,4 @@ def delete_password(id):
 
     return redirect("/dashboard")
 if __name__ == "__main__":
-    init_db()
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, port=5000)
